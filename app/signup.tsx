740efddd2415cc98a647c24b78516d1db6a02d96
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-03-19 15:50:21 -0300

    add de stylesheet no signup.tsx

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Signup = () => {
@@ -10,3 +10,14 @@ const Signup = () => {
 }
 
 export default Signup;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D9D9D9',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    fontFamily: 'Jost',
+  },
+})
