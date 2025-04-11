3b1d6be301ac79d11276a435a40d5f9c6c41f85c
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-11 10:53:51 -0300

    create signup

---
+++ app/signup.tsx
@@ -0,0 +1,18 @@
+
+import React, { Component, useEffect } from 'react'
+import 
+
+const useSchema = () =>
+const Signup = () => {
+
+return (
+     
+)
+  useEffect(() =>{
+    const obj = {};
+    const valid = useSchema.safeParse("") ;
+    const valid2 = useSchema.safeParse("");
+  })
+}
+
+export default Signup
