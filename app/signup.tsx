64e0fef868fad2aa81fe64150bfab0b257684266
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-11 11:05:05 -0300

    update package

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,18 +1,34 @@
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Link, useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import Card from '../components/Card';
 
-import React, { Component, useEffect } from 'react'
-import 
 
-const useSchema = () =>
+const useSchema = z.string();
+const userSchema = z.object({
+    name: z.string.min(2,{message:"Nome é obrigatório"}),
+    email: z.string.email({message:"Email é obrigatório"}),
+    phone:  z.string().optional(),
+    password: z
+        .string
+}
+)
 const Signup = () => {
 
 return (
      
 )
   useEffect(() =>{
-    const obj = {};
-    const valid = useSchema.safeParse("") ;
-    const valid2 = useSchema.safeParse("");
-  })
+    const obj = {name: undefined, email:"", password:""};
+    const valid = useSchema.safeParse(obj) ;
+    const valid2 = userSchema.safeParse("");
+  },[]);
+
+  function handleSignUp({name, email, password}: User){
+    signUp(name, email, password);
+
+    router.replace("/home");
+  }
 }
 
 export default Signup
